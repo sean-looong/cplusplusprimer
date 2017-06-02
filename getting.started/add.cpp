b4d6f185803f03a9811918b4f3ae019cf51eda22
sean.looong <sean.looong@outlook.com>
2017-06-02 18:21:03 +0800

    [cplusplusprimer] getting started, first cpp file
    计算两个数的和

---
+++ getting.started/add.cpp
@@ -0,0 +1,20 @@
+/*
+ * add.cpp
+ *
+ * Copyright (C) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E.Moo.
+ *
+ * Des: This file contains code from "C++ Primer, Fifth Edition"
+ */
+
+#include <iostream>
+
+int main() {
+    // prompt user to enter two numbers
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2
+        << " is " << v1 + v2 << std::endl;
+    return 0;
+}
+
