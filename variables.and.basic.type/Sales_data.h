b7d1f35286a2aff2d33ac84ba72923fc8475a466
sean.looong <sean.looong@outlook.com>
2017-07-04 00:36:11 +0800

    [cplusplusprimer] define data struct

---
+++ variables.and.basic.type/Sales_data.h
@@ -0,0 +1,21 @@
+/*
+ * Sales_data.h
+ *
+ * Copyright (C) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E.Moo.
+ *
+ * Des: This file contains code from "C++ Primer, Fifth Edition"
+ */
+
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+#endif
+
