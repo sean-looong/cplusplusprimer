a33df79ae8a2a1b02e1c28725faa7d963f399795
sean.looong <sean.looong@outlook.com>
2017-08-25 19:15:11 +0800

    [cpluscplusprimer] object-oriented programming

---
+++ object-oriented.programming/Query.cpp
@@ -0,0 +1,73 @@
+/*
+ * Query.cpp
+ *
+ * Copyright (C) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E.Moo.
+ *
+ * Des: This file contains code from "C++ Primer, Fifth Edition"
+ */
+
+#include <memory>
+using std::shared_ptr; using std::make_shared;
+
+#include <algorithm>
+using std::set_intersection;
+
+#include <cstddef>
+using std::size_t;
+
+#include <set>
+using std::set;
+
+#include <iostream>
+using std::ostream; using std::endl; using std::cout;
+
+#include "Query.h"
+#include "TextQuery.h"
+
+QueryResult NotQuery::eval(const TextQuery& text) const {
+    // virtual call to eval through the Query operand
+    auto result = query.eval(text);
+    // start out with an empty result set
+    auto ret_lines = make_shared<set<line_no>>();
+    // we have to iterate through the lines on which our operand appears
+    auto beg = result.begin(), end = result.end();
+    // for each line in the input file, if that line is not in result,
+    // add that line number to ret_lines
+    auto sz = result.get_file()->size();
+    for (size_t n = 0; n != sz; ++n) {
+        if (beg == end || *beg != n) {
+            ret_lines->insert(n);
+        } else if (beg != end) {
+            ++beg;
+        }
+    }
+
+    return QueryResult(rep(), ret_lines, result.get_file());
+}
+
+QueryResult AndQuery::eval(const TextQuery& text) const {
+    // virtual calls through the Query operands to get result sets for the operands
+    auto left = lhs.eval(text), right = rhs.eval(text);
+    // start out with an empty result set
+    auto ret_lines = make_shared<set<line_no>>();
+
+    // writes the intersection of two ranges to a destination iterator
+    // destination iterator in this call adds elements to ret
+    set_intersection(left.begin(), left.end(),
+                     right.begin(), right.end(),
+                     inserter(*ret_lines, ret_lines->begin()));
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
+QueryResult OrQuery::eval(const TextQuery& text) const {
+    // virtual calls through the Query operands to get result sets for the operands
+    auto left = lhs.eval(text), right = rhs.eval(text);
+    // start out with an empty result set
+    auto ret_lines = make_shared<set<line_no>>(left.begin(), left.end());
+
+    // insert lines from the right-hand operand
+    ret_lines->insert(right.begin(), right.end());
+    // return the new QueryResult representing the union of lhs and rhs
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
